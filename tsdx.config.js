bf6877032ac48343c9228a4bf635f3933d9e96e0
Ведерников Михаил Юрьевич <vedernikov@tochka.com>
2019-12-09 11:41:20 +0500

    fix: added forgotten rxjs as peer dependency

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,7 +1,10 @@
+const packageJson = require('./package.json');
+const peerDependencies = Object.keys(packageJson.peerDependencies || {}) || [];
+
 module.exports = {
-    rollup(config, options) {
+    rollup(config) {
         const oldExternalFilter = config.external;
-        config.external = (id, parentId, isResolved) => id === 'react' || oldExternalFilter(id, parentId, isResolved);
+        config.external = (id, parentId, isResolved) => peerDependencies.includes(id) || oldExternalFilter(id, parentId, isResolved);
 
         return config;
     }
