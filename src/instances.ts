65f61b16e2a01d322743facdc0d769bbaed09613
Vedernikov Mikhail <misha.smert@gmail.com>
2019-12-16 12:08:57 +0500

    fix: force initial value to field before subscribing to it's value$

--- src/instances.ts
+++ src/instances.ts
@@ -191,16 +191,16 @@ export class Formeer<Values extends Record<string, any> = any> {
     };
 
     registerField<Value = any>(fieldInstance: FormeerField<Value>): void {
+        if (this.initializeValues) {
+            fieldInstance.setValue(get(this.setValues$.value, fieldInstance.name, void 0));
+        }
+
         const subscription = fieldInstance.value$.subscribe(
             (value: Value | undefined) => this.setFieldValue(fieldInstance.name, value)
         );
 
         this.subscriptions.push(subscription);
         this.setFieldNames$.next(this.setFieldNames$.value.concat(fieldInstance.name));
-
-        if (this.initializeValues) {
-            fieldInstance.setValue(get(this.setValues$.value, fieldInstance.name, void 0));
-        }
     }
 
     private setFieldValue<Value>(name: string, value: Value) {
