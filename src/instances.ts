186dd51170229e8fb3a6902d103299b8396ed479
Vedernikov Mikhail <misha.smert@gmail.com>
2019-12-16 11:29:05 +0500

    feat: push form field's value from values on field registration

--- src/instances.ts
+++ src/instances.ts
@@ -1,3 +1,4 @@
+import get from 'lodash/set';
 import set from 'lodash/set';
 import { SyntheticEvent } from 'react';
 import { combineLatest, BehaviorSubject, Observable, Subscription } from 'rxjs';
@@ -194,6 +195,8 @@ export class Formeer<Values extends Record<string, any> = any> {
 
         this.subscriptions.push(subscription);
         this.setFieldNames$.next(this.setFieldNames$.value.concat(fieldInstance.name));
+
+        fieldInstance.setValue(get<Value>(this.setValues$.value, fieldInstance.name, void 0));
     }
 
     private setFieldValue<Value>(name: string, value: Value) {
